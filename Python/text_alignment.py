39795e42894563d826e2f8094a0693d7c81cef7d
jahanzebNawaz <jahanzaib.ch9996@gmail.com>
2023-01-22 23:05:13 +0100

    text alignment string

---
+++ Python/text_alignment.py
@@ -0,0 +1,38 @@
+
+from unittest import TestCase
+
+
+class Solution:
+    
+    def string_validate(self, thickness) :
+        #Replace all ______ with rjust, ljust or center. 
+
+        c = 'H'
+
+        #Top Cone
+        for i in range(thickness):
+            print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+
+        #Top Pillars
+        for i in range(thickness+1):
+            print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+
+        #Middle Belt
+        for i in range((thickness+1)//2):
+            print((c*thickness*5).center(thickness*6))    
+
+        #Bottom Pillars
+        for i in range(thickness+1):
+            print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))    
+
+        #Bottom Cone
+        for i in range(thickness):
+            print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
+
+
+class TestSolution(TestCase):
+    def setUp(self):
+        self.solution = Solution()
+    
+    def test_one(self):
+        result = self.solution.string_validate(thickness=5)
